b34ee1a4a385ba753802fcb26c17d19ea72bc71f
ricardo0228 <38351207+ricardo0228@users.noreply.github.com>
2019-06-13 20:04:20 +0800

    ribbon负载均衡
    通过设置RibbonLBConfig自定义负载均衡策略

---
+++ eureka/ribbon-consumer-service/src/main/java/org/ribbon/consumer/service/IndexController.java
@@ -0,0 +1,37 @@
+package org.ribbon.consumer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @Autowired private CalculateFeignService feignService;
+
+    @RequestMapping(value = "/hello" , method = RequestMethod.GET)
+    public String hello(String name){
+        return feignService.hello(name);
+    }
+    
+    @RequestMapping(value = "/add" , method = RequestMethod.GET)
+    public String add(int a, int b){
+        return feignService.Add(a, b);
+    }
+
+    @RequestMapping(value = "/minus" , method = RequestMethod.GET)
+    public String minus(int a, int b){
+        return feignService.Minus(a, b);
+    }
+
+    @RequestMapping(value = "/times" , method = RequestMethod.GET)
+    public String times(int a, int b){
+        return feignService.Times(a, b);
+    }
+
+    @RequestMapping(value = "/divide" , method = RequestMethod.GET)
+    public String divide(int a, int b){
+        return feignService.Divide(a, b);
+    }
+}
