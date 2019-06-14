4547b981142785a6766da69aaa2955eb701eec4a
ricardo0228 <38351207+ricardo0228@users.noreply.github.com>
2019-06-14 11:28:45 +0800

    整合ribbon实现负载均衡

---
+++ eureka/ribbon-consumer-service/src/main/java/org/ribbon/consumer/service/CalculateFeignService.java
@@ -0,0 +1,29 @@
+package org.ribbon.consumer.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "provider-service")
+public interface CalculateFeignService {
+
+    @RequestMapping(value = "/hello" , method = RequestMethod.GET)
+    String hello(@RequestParam("name") String name);
+    
+    @RequestMapping(value = "/add" , method = RequestMethod.GET)
+    String Add(@RequestParam("a") int a,
+            @RequestParam("b") int b);
+
+    @RequestMapping(value = "/minus" , method = RequestMethod.GET)
+    String Minus(@RequestParam("a") int a,
+              @RequestParam("b") int b);
+
+    @RequestMapping(value = "/times" , method = RequestMethod.GET)
+    String Times(@RequestParam("a") int a,
+              @RequestParam("b") int b);
+
+    @RequestMapping(value = "/divide" , method = RequestMethod.GET)
+    String Divide(@RequestParam("a") int a,
+              @RequestParam("b") int b);
+}
